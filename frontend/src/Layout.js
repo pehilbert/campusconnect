1e0e3f2fbf67853dc65f336d7863bbe668ecc207
pehilbert <148654775+pehilbert@users.noreply.github.com>
2024-10-02 15:44:47 -0700

    Adjustments

--- frontend/src/Layout.js
+++ frontend/src/Layout.js
@@ -2,17 +2,15 @@ import { Outlet, Link } from "react-router-dom";
 
 function Layout() {
     return (
-        <div className="Layout-container">
-            <div className="Layout">
-                <div className="nav-bar">
-                    <h1 className="title">CampusCorner</h1>
-                    <div className="links">
-                        <Link className="nav-link" to="/">Home</Link>
-                    </div>
+        <div className="Layout">
+            <div className="nav-bar">
+                <h1 className="title">CampusCorner</h1>
+                <div className="links">
+                    <Link className="nav-link" to="/">Home</Link>
                 </div>
-
-                <Outlet />
             </div>
+
+            <Outlet />
         </div>
     );
 }
